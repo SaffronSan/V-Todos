3f24aa9fd8c7ca76a13685559ab9909fd6b501d6
Anish Thapa <94629230+SaffronSan@users.noreply.github.com>
2025-06-26 11:02:22 -0400

    test 3843204

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,5 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === '/V-Tac/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/V-Tac/' : ''
 });
